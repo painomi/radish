4504028e015051e09fd2240498eade580865ce2a
xex-ken-xex <kenichi.izawa@gmail.com>
2019-03-23 23:28:28 +0900

    add vue settings

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,4 +1,10 @@
 const { environment } = require('@rails/webpacker')
+const config = environment.toWebpackConfig()
+config.resolve.alias = {
+    // Vue の runtime build を有効にする
+    'vue$': 'vue/dist/vue.esm.js'
+}
+
 const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
 
