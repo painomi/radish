3044d1f3c5b3fa90f078580e54266a93274f7423
painomi <painomi.ringo@gmail.com>
2019-03-31 12:52:45 +0900

    動かないやつ

--- Gemfile
+++ Gemfile
@@ -7,7 +7,6 @@ gem 'rails', '~> 5.2.2', '>= 5.2.2.1'
 gem 'pg', '>= 0.18', '< 2.0'
 gem 'puma', '~> 3.11'
 gem 'haml-rails'
-gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 gem 'jbuilder', '~> 2.5'
@@ -39,7 +38,8 @@ gem 'whenever', :require => false   # cronを登録できる
 gem 'exception_notification'   # エラーが発生したらメールを送ってくれる
 gem 'newrelic_rpm'             # パフォーマンス監視サービスを利用できる
 gem 'simple_form'
-gem 'bootstrap', '~> 4.1.1'
+gem 'bootstrap-sass'
+gem 'sass-rails'
 gem 'jquery-rails'
 
 group :development, :test do
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_21_081608) do
+ActiveRecord::Schema.define(version: 2019_03_30_043432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "people", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "taggings", id: :serial, force: :cascade do |t|
     t.integer "tag_id"
